fc364d5680e1b16b3b63504d6a26674092d7c42c
xRadne <32938476+xRadne@users.noreply.github.com>
2021-09-26 17:31:03 +0200

    Initial commit

---
+++ ExplodingWhale/ExplodingWhaleComponent.cs
@@ -0,0 +1,156 @@
+﻿using Grasshopper.Kernel;
+using Rhino.Geometry;
+using System;
+using System.Linq;
+using System.Reflection;
+using System.Collections.Generic;
+using System.Windows.Forms;
+
+// In order to load the result of this wizard, you will also need to
+// add the output bin/ folder of this project to the list of loaded
+// folder in Grasshopper.
+// You can use the _GrasshopperDeveloperSettings Rhino command for that.
+
+namespace ExplodingWhale
+{
+    public class ExplodingWhaleComponent : GH_Component, IGH_VariableParameterComponent
+    {
+        /// <summary>
+        /// Each implementation of GH_Component must provide a public 
+        /// constructor without any arguments.
+        /// Category represents the Tab in which the component will appear, 
+        /// Subcategory the panel. If you use non-existing tab or panel names, 
+        /// new tabs/panels will automatically be created.
+        /// </summary>
+        public ExplodingWhaleComponent()
+          : base("ExplodingWhale", "Explode",
+              "Explode any object into its fields and properties",
+              "Params", "Util")
+        {
+        }
+
+        /// <summary>
+        /// Registers all the input parameters for this component.
+        /// </summary>
+        protected override void RegisterInputParams(GH_InputParamManager pManager)
+        {
+            pManager.AddGenericParameter("Object", "O", "Object to explode into its fields and properties", GH_ParamAccess.item);
+        }
+
+        /// <summary>
+        /// Registers all the output parameters for this component.
+        /// </summary>
+        protected override void RegisterOutputParams(GH_OutputParamManager pManager)
+        {
+            pManager.AddTextParameter("Fields", "F", "Fields and properties of object", GH_ParamAccess.list);
+            pManager.AddGenericParameter("Values", "V", "Values of fields and properties of object", GH_ParamAccess.list);
+        }
+
+        /// <summary>
+        /// This is the method that actually does the work.
+        /// </summary>
+        /// <param name="DA">The DA object can be used to retrieve data from input parameters and 
+        /// to store data in output parameters.</param>
+        protected override void SolveInstance(IGH_DataAccess DA)
+        {
+            //DA.AbortComponentSolution();
+
+            object obj = null;
+            if (!DA.GetData("Object", ref obj)) return;
+
+            var type = obj.GetType();
+            
+            var fields = type.GetFields(BindingFlags.Public | BindingFlags.Instance);
+            var properties = type.GetProperties(BindingFlags.Public | BindingFlags.Instance).Where(p => p.CanRead);
+
+            var fieldNames = fields.Select(f => f.Name);
+            var propertiesNames = properties.Select(p => p.Name);
+            var fieldsAndPropertiesNames = fieldNames.Concat(propertiesNames);
+
+            var fieldValues = fields.Select(f => f.GetValue(f));
+            var propertyValues = properties.Select(p => p.GetValue(p));
+            var fieldsAndPropertiesValues = fieldValues.Concat(propertyValues);
+
+            DA.SetDataList("Fields", propertiesNames);
+            DA.SetDataList("Values", fieldValues);
+        }
+
+        public bool CanInsertParameter(GH_ParameterSide side, int index)
+        {
+            return false;
+        }
+
+        public bool CanRemoveParameter(GH_ParameterSide side, int index)
+        {
+            if (side == GH_ParameterSide.Input)
+                return false;
+            else
+                return true;
+        }
+
+        public IGH_Param CreateParameter(GH_ParameterSide side, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool DestroyParameter(GH_ParameterSide side, int index)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void VariableParameterMaintenance()
+        {
+            
+        }
+
+        public override void AppendAdditionalMenuItems(ToolStripDropDown menu)
+        {
+            var item1 = Menu_AppendItem(menu, "Item 1", Menu_Clicked, true, true);
+            item1.ToolTipText = "Tooltip for item 1";
+            item1.Name = "Item 1";
+
+            var item2 = new ToolStripMenuItem("Item 2", null, (sender, e) => { });
+            item2.Name = "Item 2";
+            menu.Items.Add(item2);
+
+            Menu_AppendItem(menu, "Public", Menu_Clicked, true, true);
+            Menu_AppendItem(menu, "Private", Menu_Clicked, true, true);
+            Menu_AppendItem(menu, "Static", Menu_Clicked, true, true);
+            Menu_AppendItem(menu, "Instance", Menu_Clicked, true, true);
+
+            base.AppendAdditionalMenuItems(menu);
+        }
+
+        private void Menu_Clicked(object sender, EventArgs e)
+        {
+            var menu = sender as ToolStripMenuItem;
+            //menu.Checked = !menu.Checked;
+
+            Rhino.RhinoApp.WriteLine($"Clicked: {menu.Name}, Checked={menu.Checked}");
+        }
+
+        /// <summary>
+        /// Provides an Icon for every component that will be visible in the User Interface.
+        /// Icons need to be 24x24 pixels.
+        /// </summary>
+        protected override System.Drawing.Bitmap Icon
+        {
+            get
+            {
+                // You can add image files to your project resources and access them like this:
+                //return Resources.IconForThisComponent;
+                return null;
+            }
+        }
+
+        /// <summary>
+        /// Each component must have a unique Guid to identify it. 
+        /// It is vital this Guid doesn't change otherwise old ghx files 
+        /// that use the old ID will partially fail during loading.
+        /// </summary>
+        public override Guid ComponentGuid
+        {
+            get { return new Guid("26d4b024-0795-49aa-bcc6-39a6c2e04ca8"); }
+        }
+    }
+}
---
+++ ExplodingWhale/ExplodingWhaleInfo.cs
@@ -0,0 +1,57 @@
+﻿using Grasshopper.Kernel;
+using System;
+using System.Drawing;
+
+namespace ExplodingWhale
+{
+    public class ExplodingWhaleInfo : GH_AssemblyInfo
+    {
+        public override string Name
+        {
+            get
+            {
+                return "ExplodingWhale";
+            }
+        }
+        public override Bitmap Icon
+        {
+            get
+            {
+                //Return a 24x24 pixel bitmap to represent this GHA library.
+                return null;
+            }
+        }
+        public override string Description
+        {
+            get
+            {
+                //Return a short string describing the purpose of this GHA library.
+                return "";
+            }
+        }
+        public override Guid Id
+        {
+            get
+            {
+                return new Guid("27fd3b34-03d0-4edd-a9d0-c672abc1ea49");
+            }
+        }
+
+        public override string AuthorName
+        {
+            get
+            {
+                //Return a string identifying you or your company.
+                return "";
+            }
+        }
+        public override string AuthorContact
+        {
+            get
+            {
+                //Return a string representing your preferred contact details.
+                return "";
+            }
+        }
+    }
+}
